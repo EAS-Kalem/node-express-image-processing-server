ce81162ac9e296f77fdd1048a8730198b3424c7f
admin <kalem@enterpriseautomation.co.uk>
2022-04-12 11:01:44 +0100

    3

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
-const pathToIndex = '../client/index.html';
+const pathToIndex = path.resolve(__dirname, '../client/index.html' );
 const app = express();
-path.resolve(__dirname, '../client/index.html' ),
+
 
 app.use('/*', (req, res ) => {
     sendFile(res, pathToIndex)
+
+
 })
 
 
