fbafd20b71fdeb3d69cec6576e29c6680edf602c
admin <kalem@enterpriseautomation.co.uk>
2022-04-12 11:38:59 +0100

    5

--- api/src/router.js
+++ api/src/router.js
@@ -1,15 +1,40 @@
-const express = require('express');
+const { Router } = require('express');
 const multer = require('multer');
-const { request } = require('../app');
+const { response } = require('../app');
+
 
 router = require(express)
 
-const router = router()
-filename((request, file, callback) =>{
-    callback(null, file.originalName)
-})
+const router = Router();
 
-const storage = diskStorage(multer)
-destination = 'api/uploads/'
-filename = filename()
+const filename = ((request, file, callback) => {
+    callback(null, file.originalName);
+});
+
+const storage = multer.diskStorage({
+    destination: 'api/uploads/',
+    filename,
+
+})
+const fileFilter = (request, file, calllback) => {
+    if (file.mimetype !== 'image/png') {
+        request.fileValidationError = 'Wrong File Type';
+        callback(null, false, new Error('Wrong File Type'));
+    } else {
+        callback(null,true)
+    }
+};
+
+const upload = multer ({
+    fileFilter,
+    storage,
+});
+
+router.post('/upload', upload.single('photo'), (request, resolve) => {
+    if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError})
+
+    return response.status(201).json({success: true});
+
+});
+
 module.exports = router;
